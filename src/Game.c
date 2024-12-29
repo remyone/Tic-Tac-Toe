61e0ac29b617f2f8886c1e1847bb87fbb3e41e5b
remy <oneremy76@gmail.com>
2024-12-29 10:18:36 +0400

    Curtailed code in main file

--- src/Game.c
+++ src/Game.c
@@ -9,13 +9,8 @@ int main()
 						 {'-', '-', '-'} };
 	printBoard(board);
 	bool isWorking = true;
-	short k;
+	short k, l, row, column, ForB, move;
 	short *p_k = &k;
-	short l;
-	short row;
-	short column;
-	short ForB;
-	short move;
 	printf("1.play with friend or 2.play with bot?(choose 1 or 2): ");
 	scanf_s("%hd", &ForB);
 	if (ForB == 1)
